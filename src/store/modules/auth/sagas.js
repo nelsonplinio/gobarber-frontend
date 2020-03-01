ec479a8313ba0fa98466fd06e8eef1132a287048
Nelson Plíni <nelsonplinio94@gmail.com>
2020-02-29 21:51:36 -0300

    Front End: Melhoria nas mensagens de erro

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,6 +1,8 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
 // takeLastes observa uma action do redux e redireciona ela para a função que e o segundo parametro.
 
+import { toast } from 'react-toastify';
+
 import api from '~/services/api';
 import history from '~/services/history';
 
@@ -18,7 +20,7 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('User is not a provider');
+      toast.error('User is not a provider');
       return;
     }
 
@@ -26,6 +28,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (error) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
